e6e5d3c9ac79e78a89c74de3a06a11163cf8f881
Antonyj26 <antonyjosu26@gmail.com>
2026-04-03 19:39:08 -0300

    chore: initial setup (Spring Boot, JPA, PostgreSQL)

---
+++ src/test/java/br/com/almeida/louvor_manager_api/LouvorManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.almeida.louvor_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouvorManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
